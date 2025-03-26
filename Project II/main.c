fa156e00be183c637f9bca314348052e130cb382
Rasik Pandit <Rpandit3867@conestogac.on.ca>
2025-03-26 19:36:35 -0400

    chnages in main.c

--- Project II/main.c
+++ Project II/main.c
@@ -1,26 +1,7 @@
 #include <stdio.h>
-#include"Welcome.h"
+#include "main.h"
 
-int main()
-{
-    printf("Hello\n");
-
-        int userChoice;
-
-        // Display the welcome screen
-        displayWelcomeMessage();
-
-        // Loop until the user chooses to exit
-        while (1) 
-        {
-            userChoice = getUserSelection();
-            if (userChoice == 4)
-            {
-                navigateSelection(userChoice);
-                break; // Exit the loop
-            }
-            navigateSelection(userChoice);
-        }
-
-        return 0;
- }
+int main() {
+    printf("Hello!!!\n");
+    return 0;
+}
